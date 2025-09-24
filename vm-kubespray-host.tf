6260cbdedb58ae5131cb8d975ae32087dd013044
Martin Stiehr <martin.stiehr.extern@bdr.de>
2025-09-24 15:08:40 +0200

    Increase memory of kubespray machine

--- vm-kubespray-host.tf
+++ vm-kubespray-host.tf
@@ -57,7 +57,7 @@ module "kubespray_host" {
   pm_hosts                     = var.pm_hosts
   vm_ubuntu_tmpl_name          = var.vm_ubuntu_tmpl_name
   vm_name_prefix               = var.use_legacy_naming_convention ? "${var.env_name}-kubespray" : "vm-${local.cluster_name}-kubespray"
-  vm_memory_mb                 = 2048
+  vm_memory_mb                 = 4096
   vm_cpu_cores                 = var.vm_cpu_cores
   vm_os_disk_storage           = var.vm_os_disk_storage
   vm_os_disk_size_gb           = 10
